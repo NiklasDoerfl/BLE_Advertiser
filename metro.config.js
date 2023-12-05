9eaf3268d4abf19c082072cfa9b34b945b79bbc1
Niklas Doerfl <niklas.doerfl@htl-braunau.at>
2023-12-05 08:43:28 +0100

    V3 published alte Version warum auch immer

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 module.exports = mergeConfig(getDefaultConfig(__dirname), {
     server: {
-      port: 808, // Change the port number to your desired value
+      port: 1006, // Change the port number to your desired value
     },
 });
